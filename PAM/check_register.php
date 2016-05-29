ba37280c2633e5e4aaabb3628f1d5ee3e3c01904
curiositywzq <wu-zhiqiang@hotmail.com>
2016-05-29 17:29:26 +0800

    check_register.php更新

--- PAM/check_register.php
+++ PAM/check_register.php
@@ -5,9 +5,10 @@
  * Date: 2016/5/28
  * Time: 15:35
  */
+require_once("common/db.php");
 $conn = db_connect();
 $email = isset($_GET['email']) ? htmlspecialchars($_GET['email']) : '';
-$check1 ="select * from `buyer` where `email`= '".$email."'";
+$check1 ="select * from buyer where `email`= '".$email."'";
 $set1 = mysqli_query($conn,$check1,MYSQLI_STORE_RESULT);
 $result1=mysqli_fetch_array($set1,MYSQLI_ASSOC);
 $check2 ="select * from `seller` where `email`= '".$email."'";

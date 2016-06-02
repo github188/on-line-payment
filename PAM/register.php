24a7f13ab7fc5f5bc7db408f27def292c0434ade
curiositywzq <wu-zhiqiang@hotmail.com>
2016-06-02 23:47:11 +0800

    注册php调试，邮件可发送但还未验证

--- PAM/register.php
+++ PAM/register.php
@@ -7,6 +7,7 @@
  */
 header("Content-Type: text/plain;charset=utf-8");
 require_once('common/db.php');
+require_once "../common/email.class.php";
 session_start();
 
 $realName =$_POST['name'];
@@ -15,7 +16,7 @@ $email = $_POST['email'];
 $ID = $_POST['identity'];
 $password = $_POST['password'];
 $password2 = $_POST['repassword'];
-
+$ifsuccess=0;
 $conn = db_connect();
 if(!empty($realName) && !empty($userType) && !empty($email) && !empty($ID) && !empty($password) && !empty($password2))
 {
@@ -51,6 +52,7 @@ if(!empty($realName) && !empty($userType) && !empty($email) && !empty($ID) && !e
                 }
                 mysqli_query($conn, "insert into IDauthReq values( $uid,'$userType','$realName','$ID',0)");
                 echo "success";
+                $ifsuccess=1;
             }
             else{
                 echo "fail";
@@ -79,10 +81,30 @@ if(!empty($realName) && !empty($userType) && !empty($email) && !empty($ID) && !e
                 }
                 mysqli_query($conn, "insert into IDauthReq values( $uid,'$userType','$realName','$ID',0)");
                 echo "success";
+                $ifsuccess=1;
             }
             else{
                 echo "fail";
             }
         }
     }
+}
+if($ifsuccess==1){
+    $smtpserver = "smtp.163.com";//SMTP服务器
+    $smtpserverport =25;//SMTP服务器端口
+    $smtpusermail = "qqq1051814353@163.com";//SMTP服务器的用户邮箱
+    $smtpemailto = $email;//发送给谁
+    $fromName = 'yuantu';
+    $smtpuser = "qqq1051814353@163.com";//SMTP服务器的用户帐号
+    $smtppass = "qqq1051814353";//密码，或者授权码
+    $mailtitle = "this is test mail";//邮件主题
+    $mailcontent = "hello";//邮件内容
+    $mailtype = "HTML";//邮件格式（HTML/TXT）,TXT为文本邮件
+    $smtp = new smtp($smtpserver,$smtpserverport,true,$smtpuser,$smtppass);//这里面的一个true是表示使用身份验证,否则不使用身份验证.
+    $smtp->debug = false;//是否显示发送的调试信息，默认不发送
+    $state = $smtp->sendmail($smtpemailto, $smtpusermail, $fromName, $mailtitle, $mailcontent, $mailtype);
+    if($state=="")
+        $error = "激活邮件发送失败，请确认你的邮箱地址！";
+    else
+        $error = "成功发送邮件！";
 }

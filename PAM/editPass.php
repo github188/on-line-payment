df791015b80b11d1bb8cc794dae74da67523ccbd
singleghost <570111267@qq.com>
2016-06-04 15:43:56 +0800

    add editPass

--- PAM/editPass.php
+++ PAM/editPass.php
@@ -16,11 +16,32 @@ if($_SESSION['login'] === true) {
     $newPassRe = $_POST['newPassRe'];
 
     if(!empty($oldPassIn) && !empty($newPass) && !empty($newPassRe)) {
+        if($newPass !== $newPassRe) {
+            die("两次新密码不一致");
+        }
         if($_SESSION['userType'] === 'seller') {
             $sql = "select password from seller WHERE s_id='" . $_SESSION['uid'] . "'";
         } elseif ($_SESSION['userType'] === 'buyer') {
             $sql = 'select password from buyer WHERE b_id="' . $_SESSION['uid'] . '"';
         }
-        mysqli_query($conn, )
+        $result = mysqli_query($conn, $sql);
+        if($result) {
+            //旧密码相等
+            if($_SESSION['userType'] === 'seller') {
+                $user_id = $_SESSION['uid'];
+                $sql = "update seller set password=$newPass WHERE s_id=$user_id";
+            } elseif ($_SESSION['userType'] === 'buyer') {
+                $user_id = $_SESSION['uid'];
+                $sql = "update buyer set password=$newPass WHERE b_id=$user_id";
+            }
+            $res = mysqli_query($conn, $sql);
+            if($res) {
+                echo "update success";
+            } else {
+                echo "update fail";
+            }
+        } else {
+            echo "旧密码不相等";
+        }
     }
 }

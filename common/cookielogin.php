5ab7bfe6c8ba9aa20eceaa2bea2cd3a2803b2092
singleghost <570111267@qq.com>
2016-06-05 14:46:59 +0800

    实现请记住我功能

--- common/cookielogin.php
+++ common/cookielogin.php
@@ -21,24 +21,27 @@ function verify_cookie()
         $sql = "select * from buyer WHERE rememberMe_id=$identifier";
     }
     $result = mysqli_query($conn, $sql);
-    $row = mysqli_fetch_array($result);
-    $email = $row['email'];
-//        $id = md5($salt . md5($email . $salt));
-    if ($token !== $row['token']) {
-        //ｃｏｏｋｉｅ认证失败
-        header("Location: ../PAM/login.html");
+    if($result) {
+        $row = mysqli_fetch_array($result);
 
-    } elseif (time() > $row['timeout']) {
-        header("Location: ../PAM/login.html");
+        if ($token !== $row['token']) {
+            //ｃｏｏｋｉｅ认证失败
+            header("Location: ../PAM/login.html");
 
-    } elseif (md5($salt . md5($row['email'], $salt)) !== $identifier) {
-        header("Location: ../PAM/login.html");
+        } elseif (time() > $row['timeout']) {
+            header("Location: ../PAM/login.html");
 
-    } elseif ($token === $row['token']) {
-        $_SESSION['login'] = true;
-        $_SESSION['email'] = $row['email'];
-        $_SESSION['uid'] = $userType === 'seller' ? $row['s_id'] : $row['b_id'];
-        $_SESSION['userType'] = $userType;
-        setcookie("username", $row['username'], time() + 60 * 60 * 24 * 30, '/');
+        } elseif (md5($salt . md5($row['email'], $salt)) !== $identifier) {
+            header("Location: ../PAM/login.html");
+
+        } elseif ($token === $row['token']) {
+            $_SESSION['login'] = true;
+            $_SESSION['email'] = $row['email'];
+            $_SESSION['uid'] = $userType === 'seller' ? $row['s_id'] : $row['b_id'];
+            $_SESSION['userType'] = $userType;
+            setcookie("username", $row['username'], time() + 60 * 60 * 24 * 30, '/');
+        }
+    } else {
+        echo "identifier not exist";
     }
 }

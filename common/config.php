1e8eef9a702186e18ac4f499d54cf22f5b0e9588
MeteorKL <1051814353@qq.com>
2016-06-15 22:52:12 +0800

    增加已取消的订单状态

--- common/config.php
+++ common/config.php
@@ -1,15 +1,16 @@
 <?php
-  // 订单状态state
-	define('STATE_MAX', 2);
+    // 订单状态state
+	define('STATE_MAX', 3);
 	define('UNPAID', 1);
 	define('PAID', 2);
+	define('CANCLED', 3);
 	
 	// 订单类型type
 	define('TYPE_MAX', 2);
 	define('FLIGHT', 1);
 	define('HOTEL', 2);
 
-  // 数据库连接信息
+    // 数据库连接信息
 	$DB_HOST = '115.159.36.21';
 	$DB_PORT = 3306;
 	$DB_USER = "payment";

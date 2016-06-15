26badc3d032a156403a76d38288d4478f53647ee
MeteorKL <1051814353@qq.com>
2016-06-15 22:29:53 +0800

    增加了订单状态state和订单类型type的宏定义

--- common/config.php
+++ common/config.php
@@ -1,8 +1,21 @@
 <?php
-$DB_HOST = "115.159.36.21";
-$DB_PORT = 3306;
-$DB_USER = "payment";
-$DB_PASSWORD = "payment123";
-$DB_SCHEMA = "payment";
-$salt = "PHPISTHEBESTLANG";
+  // 订单状态state
+	define('STATE_MAX', 2);
+	define('UNPAID', 1);
+	define('PAID', 2);
+	
+	// 订单类型type
+	define('TYPE_MAX', 2);
+	define('FLIGHT', 1);
+	define('HOTEL', 2);
+
+  // 数据库连接信息
+	$DB_HOST = '115.159.36.21';
+	$DB_PORT = 3306;
+	$DB_USER = "payment";
+	$DB_PASSWORD = 'payment123';
+	$DB_SCHEMA = 'payment';
+
+  // PHP is the best language!
+  $salt = "PHPISTHEBESTLANG";
 ?>

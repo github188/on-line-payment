3b4243187a4cc5e14034d5cc67eecf63222bcbb1
SoftDoggy <774590108@qq.com>
2016-06-19 02:04:36 +0800

    test

--- BOOKING/flight.php
+++ BOOKING/flight.php
@@ -13,6 +13,39 @@
 
 ?>
 
+        <?php 
+            $flights=array();
+            if(!isset($_GET['from'])||empty($_GET['to'])){
+            $dbc = @mysqli_connect ('localhost', 'payment', 'payment123', 'payment') OR die ('Could not connect to MySQL: ' . mysqli_connect_error() );
+                 mysqli_set_charset($dbc, 'utf8');
+                 $q= "SELECT * FROM flight NATURAL JOIN class WHERE class.discount='Y'"; 
+                 $r= @mysqli_query($dbc,$q);          
+                 while($row=mysqli_fetch_assoc($r)){    
+                        $flights[]=array("flight_number" => $row['flight_number'], "company" => $row['company'], "class_price" => $row['class_price'], 
+                          "start_time" => $row['start_time'], "arrive_time" =>$row['arrive_time'], "straight" => $row['straight'], "flight_from" => $row['flight_from'], 
+                          "flight_to" => $row['flight_to'],'classrate' => $row['classrate']);   
+                        }
+            mysqli_free_result($r);    
+            mysqli_close($dbc); 
+            }        //如果没有查询信息，就显示特价机票
+
+           else{
+                 $flight_from=$_GET['from'];
+                 $flight_to=$_GET['to'];
+                 $dbc = @mysqli_connect ('localhost', 'payment', 'payment123', 'payment') OR die ('Could not connect to MySQL: ' . mysqli_connect_error() );
+                 mysqli_set_charset($dbc, 'utf8');
+                 $q= "SELECT * FROM flight NATURAL JOIN class WHERE flight.flight_from='$flight_from' AND flight.flight_to='$flight_to'"; 
+                 $r= @mysqli_query($dbc,$q);     
+                 while($row=mysqli_fetch_assoc($r)){    
+                        $flights[]=array("flight_number" => $row['flight_number'], "company" => $row['company'], "class_price" => $row['class_price'], 
+                          "start_time" => $row['start_time'], "arrive_time" =>$row['arrive_time'], "straight" => $row['straight'], "flight_from" => $row['flight_from'], 
+                          "flight_to" => $row['flight_to'], 'classrate' => $row['classrate']);  
+                        }
+            mysqli_free_result($r);    
+            mysqli_close($dbc);
+              }      
+         ?>
+
 
 <div class="container" style="margin-top: 30px">
     <div class="row">
@@ -22,7 +55,7 @@
                 <li class="list-group-item"><h4><span class="label label-default">出发时间</span>  <input type="text"  id="startdate" value="10/24/1984" /></h4></li>
                     <li class="list-group-item"><h4><span class="label label-default">出发地点</span>  <input type="text" id="fromcity" class="from-control city-select" readonly="readonly"></h4></li>
                     <li class="list-group-item"><h4><span class="label label-default">到达地点</span> <input type="text" id="tocity" class="from-control city-select" readonly="readonly"></h4></li>
-                    <li class="list-group-item"><button class="btn btn-info" style="margin-left: 42%">查询</button></li>
+                    <li class="list-group-item"><button class="btn btn-info" style="margin-left: 42%" id="submit">查询</button></li>
                     
                 </ul>
             </div>
@@ -30,67 +63,98 @@
             <div class="btn-group" role="group">
                 <button class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> 起飞时间<span class="caret"></span></button>
                 <ul class="dropdown-menu">
-                    <li><a>上午 6:00-12:00</a></li>
-                    <li><a>下午12:00-18:00</a></li>
-                    <li><a>下午 18:00-24:00</a></li>
+                    <li><button class="btn" value="all" onclick="changeTime(this.value)" style="background-color:white">所有时间</button></li>
+                    <li><button class="btn" value="morning" onclick="changeTime(this.value)" style="background-color:white">06:00-12:00</button></li>
+                    <li><button class="btn" value="noon" onclick="changeTime(this.value)" style="background-color:white">12:00-18:00</button></li>
+                    <li><button class="btn" value="night" onclick="changeTime(this.value)" style="background-color:white">18:00-24:00</button></li>
              </ul>
             </div>
              <div class="btn-group" role="group">
                 <button class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> 航空公司<span class="caret"></span></button>
-                <ul class="dropdown-menu">
-                    <li><a>东方航空公司</a></li>
-                    <li><a>南方航空公司</a></li>
-                    <li><a>西方航空公司</a></li>
-                    <li><a>北方航空公司</a></li>
-             </ul>
+                 <ul class="dropdown-menu">
+                 <li><button class="btn" value="all" onclick="changeCompany(this.value)" style="background-color:white">所有公司</button></li>
+                    <li><button class="btn" value="四川航空" onclick="changeCompany(this.value)" style="background-color:white">四川航空</button></li>
+                    <li><button class="btn" value="九元航空" onclick="changeCompany(this.value)" style="background-color:white">九元航空</button></li>
+                    <li><button class="btn" value="中国航空" onclick="changeCompany(this.value)" style="background-color:white">中国国航</button></li>
+                    <li><button class="btn" value="南方航空" onclick="changeCompany(this.value)" style="background-color:white">南方航空</button></li>
+                    <li><button class="btn" value="成都航空" onclick="changeCompany(this.value)" style="background-color:white">成都航空</button></li>
+                    <li><button class="btn" value="上海航空" onclick="changeCompany(this.value)" style="background-color:white">上海航空</button></li>
+                    <li><button class="btn" value="长龙航空" onclick="changeCompany(this.value)" style="background-color:white">长龙航空</button></li>
+                    <li><button class="btn" value="天津航空" onclick="changeCompany(this.value)" style="background-color:white">天津航空</button></li>
+                    <li><button class="btn" value="吉祥航空" onclick="changeCompany(this.value)" style="background-color:white">吉祥航空</button></li>
+                    <li><button class="btn" value="海南航空" onclick="changeCompany(this.value)" style="background-color:white">海南航空</button></li>
+                    <li><button class="btn" value="首都航空" onclick="changeCompany(this.value)" style="background-color:white">首都航空</button></li>
+                    <li><button class="btn" value="中国联航" onclick="changeCompany(this.value)" style="background-color:white">中国联航</button></li>
+                    <li><button class="btn" value="厦门航空" onclick="changeCompany(this.value)" style="background-color:white">厦门航空</button></li>
+                    <li><button class="btn" value="东方航空" onclick="changeCompany(this.value)" style="background-color:white">东方航空</button></li>
+                    <li><button class="btn" value="山东航空" onclick="changeCompany(this.value)" style="background-color:white">山东航空</button></li>
+                    <li><button class="btn" value="西藏航空" onclick="changeCompany(this.value)" style="background-color:white">西藏航空</button></li>
+                    <li><button class="btn" value="深圳航空" onclick="changeCompany(this.value)" style="background-color:white">深圳航空</button></li>
+                    <li><button class="btn" value="扬子江航空" onclick="changeCompany(this.value)" style="background-color:white">扬子江航空</button></li>                    
+                </ul>
             </div>
             <div class="btn-group" role="group">
                 <button class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> 舱位<span class="caret"></span></button>
                 <ul class="dropdown-menu">
-                    <li><a>经济舱</a></li>
-                    <li><a>头等舱</a></li>
-                    <li><a>总统舱</a></li>
+                    <li><button class="btn" value="all" onclick="changeCangwei(this.value)" style="background-color:white">所有舱位</button></li>
+                    <li><button class="btn" value="经济舱" onclick="changeCangwei(this.value)" style="background-color:white">经济舱</button></li>
+                    <li><button class="btn" value="头等舱" onclick="changeCangwei(this.value)" style="background-color:white">头等舱</button></li>
+                    <li><button class="btn" value="商务舱" onclick="changeCangwei(this.value)" style="background-color:white">商务舱</button></li>
              </ul>
             </div>
            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <div class="btn-group" role="group">
-                <button class="btn btn-info" > 价格&nbsp;&nbsp;&nbsp;  <img src="img/down.png"></button>
-            </div>
-            <div class="btn-group" role="group">
-                <button class="btn btn-info" > 准点率&nbsp;&nbsp;&nbsp;  <img src="img/down.png"></button>
+                <button class="btn btn-info" id="price"> 价格&nbsp;&nbsp;&nbsp;  <img src="img/down.png"></button>
             </div>
 
-            <h3>特价机票</h3>(如果get参数为空特价机票)
-            <hr>
-            <div class="list-group packages" id="flights">
-                 <div class="package list-group-item" data-library-name="bootstrap">
-                  <div class="row">
-                  <div class="col-md-3 flight-company">
-                    东方航空公司
-                  </div>
-                  <div class="col-md-5">
-                          <div class="row">
-                            <div class="col-md-5">
-                            <times>8:00</times><br>
-                            杭州萧山机场
-                            </div>
-                            <div class="col-md-2 arrow">=></div>
-                            <div class="col-md-5">
-                            <times>16:45</times><br>
-                            成都双流机场
-                            </div>
-                          </div>         
-                  </div>
-                  <div class="col-md-2">
-                    准点率<br>
-                    <price>98%</price>
-                  </div>
-                  <div class="col-md-2">
-                    <price>￥498</price>起<br>
-                    <a href="order-flight.php?id=what"><button class="btn btn-warning">预订</button></a>
-                  </div>                   
-                  </div>
-                </div>
+        <?php
+        if(!isset($_GET['from'])||empty($_GET['to'])){
+          echo '<br>';
+          echo "<h3>特价机票</h3>";
+          echo '</br>';
+        }
+        ?>
+
+      <hr>
+
+
+  <div class="list-group packages" id="flights">
+        <?php
+            foreach ($flights as $flight){
+            echo '<div class="package list-group-item" data-library-name="bootstrap">';
+            echo      '<div class="row">';
+            echo      '<div class="col-md-3 flight-number"><company>';
+            echo        $flight['flight_number'];
+            echo      '<br><com>' . $flight['company'] .'</com></company></div>';
+            echo      '<div class="col-md-5">';
+            echo              '<div class="row">';
+            echo                '<div class="col-md-5">';
+            echo                '<times>'.substr($flight['start_time'],10,6).'</times><br><city>';
+            echo                $flight['flight_from'];
+            echo                '</city></div>';
+            echo                '<div class="col-md-2 arrow">=></div>';
+            echo                '<div class="col-md-5">';
+            echo                '<times>'. substr($flight['arrive_time'],10,6) .'</times><br><city>';
+            echo                $flight['flight_to'];
+            echo                '</city></div>';
+            echo              '</div>' ; 
+            echo      '</div>';
+            echo     '<div class="col-md-2"><b style="padding-left: 10%"><cangwei>';
+            echo        $flight['classrate'].'</cangwei></b><br>';
+            echo        '<price>￥'.$flight['class_price'].'</price>';
+            echo      '</div>';
+            echo      '<div class="col-md-2">';
+            echo        '<a href="order-flight.php?id=' . $flight["flight_number"] . '&cangwei=' . $flight['classrate'] . '"> <button class="btn btn-warning">预订</button></a>';
+            echo      '</div>';                   
+            echo      '</div>';
+            echo    '</div>';
+              }
+          ?>
+
+
+
+
+
  
             </div>
             <br>
@@ -104,9 +168,9 @@
               </a>
               </div>
                <div class="col-md-4">
-              <a href="flight.php?from=哈尔滨&to=海口">
+              <a href="flight.php?from=北京&to=海口">
                   <img src="img/haikou.jpg" width="200" height="121">
-                  <br>哈尔滨=>海口<br><br>
+                  <br>北京=>海口<br><br>
               </a>
               </div>
                <div class="col-md-4">
@@ -155,7 +219,6 @@
     }
     times
     {
-      padding-left: 20%;
       font-size: larger;
       font-weight: bold;
     }
@@ -175,15 +238,12 @@
       font-size: large;
       color: green;
     }
+     city,company
+    {
+        font-size: large;
+       padding-left: 5%
+    }
+    
     </style>
 
-<script type="text/javascript">
-$(function() {
-    $('#startdate').daterangepicker({
-        singleDatePicker: true,
-        showDropdowns: true,
-        startDate: moment(),
-    });
-    $('.city-select').citySelect();
-});
-</script>
+<script type="text/javascript" src="/js/flight.js"></script>
